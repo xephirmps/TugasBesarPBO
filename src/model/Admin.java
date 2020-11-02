c5436b2173e73a1748564efbfb273f2da6348df4
User <User@DESKTOP-DVC228G>
2020-11-02 13:43:35 +0700

    buat class admin

---
+++ src/model/Admin.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author User
+ */
+public class Admin extends Users{
+    boolean admin;
+    
+    public Admin(){
+        
+    }
+
+    public Admin(boolean admin, String nama, String alamat, String email, String username, String password) {
+        super(nama, alamat, email, username, password);
+        this.admin = admin;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+    
+}
