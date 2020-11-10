307819897e8b29c8d6c4d3203c772d56dd78e448
Kolis <>
2020-11-10 15:46:38 +0700

    Error Deleted

--- src/model/Pembayaran.java
+++ src/model/Pembayaran.java
@@ -12,15 +12,15 @@ import java.sql.Date;
  * @author User
  */
 public class Pembayaran extends Pesanan{
-    String metodePembayaran;
-    int diskon;
+    private String metodePembayaran;
+    private int diskon;
     
     public Pembayaran(){
         
     }
 
-    public Pembayaran(String metodePembayaran, int diskon, int idPesanan, String tipePesanan, String alamatJemput, String alamatTujuan, int totalHarga, Date tanggalPemesanan) {
-        super(idPesanan, tipePesanan, alamatJemput, alamatTujuan, totalHarga, tanggalPemesanan);
+    public Pembayaran(String metodePembayaran, int diskon, int idPesanan, String tipePesanan, String alamatJemput, String alamatTujuan, int totalHarga, String tanggalPemesanan, int status) {
+        super(idPesanan, tipePesanan, alamatJemput, alamatTujuan, totalHarga, tanggalPemesanan, status);
         this.metodePembayaran = metodePembayaran;
         this.diskon = diskon;
     }
